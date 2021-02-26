5744aaf2d3d05d92ed5f33c6d05efaefbd8f4e77
afedoseev <afedoseev@AFEDOSEEV>
2021-02-26 18:03:53 +0300

    Add usb interface

---
+++ csp/share/usb.hpp
@@ -0,0 +1,20 @@
+#ifndef CSP_SHARE_USB
+#define CSP_SHARE_USB
+
+namespace csp::usb
+{
+enum class Number
+{
+    _1 = 0,
+    _2,
+    _3
+};
+
+enum class Speed
+{
+    full_speed = 0,
+    high_speed
+};
+}
+
+#endif /* CSP_SHARE_USB */
