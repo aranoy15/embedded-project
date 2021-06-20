2c0ae0d5bbe9d6beedbc0b47f07b54c770cb516c
Anton Fedoseev <aranoy15@gmail.com>
2021-06-20 17:33:47 +0300

    add contains, erase and equal functions and unit tests for that

--- src/lib/data/string.hpp
+++ src/lib/data/string.hpp
@@ -14,14 +14,14 @@ namespace data
 {
 
     template <std::size_t length>
-    class string final
+    class string
     {
     public:
         static const char TERMINATE = '\0';
 
     public:
         string() noexcept;
-        string(const char* str) noexcept;
+        explicit string(const char* str) noexcept;
         template <std::size_t copy_length>
         explicit string(const string<copy_length>& other) noexcept;
         ~string() = default;
@@ -31,7 +31,7 @@ namespace data
         std::size_t free() const noexcept { return length - _size; }
 
         bool is_full() const noexcept { return _size == length; }
-        [[maybe_unused]] bool is_empty() const noexcept { return _size == 0; }
+        bool is_empty() const noexcept { return _size == 0; }
         void clear() noexcept { _size = 0; }
 
         const char* as_str() const noexcept { return _data; }
@@ -46,6 +46,23 @@ namespace data
         template <std::size_t append_length>
         void append(string<append_length>& str) noexcept;
 
+        bool starts_with(char ch) noexcept;
+        bool starts_with(const char* str) noexcept;
+        template <std::size_t starts_length>
+        bool starts_with(const string<starts_length>& str) noexcept;
+
+        bool contains(char ch) noexcept;
+        bool contains(const char* str) noexcept;
+        template <std::size_t contains_length>
+        bool contains(const string<contains_length>& str) noexcept;
+
+        bool equal(char ch) noexcept;
+        bool equal(const char* str) noexcept;
+        template<std::size_t equal_length>
+        bool equal(const string<equal_length>& str) noexcept;
+
+        void erase(std::size_t start, std::size_t size) noexcept;
+
         template <std::size_t copy_length>
         string& operator=(const string<copy_length>& other) noexcept;
         string& operator=(const char* str) noexcept;
@@ -58,6 +75,7 @@ namespace data
     private:
         void terminate() noexcept;
         void copy(const char* data, std::size_t size) noexcept;
+        bool contains(const char* data, std::size_t size) noexcept;
     };
 
     template <std::size_t length>
@@ -86,7 +104,7 @@ namespace data
     {
         if (is_full()) return;
 
-        _data[_size + 1] = TERMINATE;
+        _data[_size] = TERMINATE;
     }
 
     template <std::size_t length>
@@ -159,6 +177,113 @@ namespace data
         copy(str, len);
         return *this;
     }
+    template <std::size_t length>
+    bool string<length>::starts_with(char ch) noexcept
+    {
+        if (is_empty()) return false;
+
+        return _data[0] == ch;
+    }
+    template <std::size_t length>
+    bool string<length>::starts_with(const char* str) noexcept
+    {
+        if (is_empty()) return false;
+
+        std::size_t len = strlen(str);
+
+        if (size() < len) return false;
+
+        return strncmp(as_str(), str, len) == 0;
+    }
+    template <std::size_t length>
+    template <std::size_t starts_length>
+    bool string<length>::starts_with(const string<starts_length>& str) noexcept
+    {
+        if (is_empty() or str.is_empty()) return false;
+
+        return strncmp(as_str(), str.as_str(), str.size()) == 0;
+    }
+    template <std::size_t length>
+    bool string<length>::contains(char ch) noexcept
+    {
+        return contains(&ch, sizeof(ch));
+    }
+    template <std::size_t length>
+    bool string<length>::contains(const char* data, std::size_t size) noexcept
+    {
+        if (size == 0 or is_empty()) return false;
+
+        for (std::size_t i = 0; i < this->size(); ++i) {
+            if (_data[i] == data[0]) {
+                if (this->size() >= (i + size))
+                    return strncmp(&_data[i], data, size) == 0;
+                else return false;
+            }
+        }
+
+        return false;
+    }
+    template <std::size_t length>
+    bool string<length>::contains(const char* str) noexcept
+    {
+        std::size_t len = strlen(str);
+
+        if (len == 0 or is_empty()) return false;
+
+        return contains(str, len);
+    }
+    template <std::size_t length>
+    template <std::size_t contains_length>
+    bool string<length>::contains(const string<contains_length>& str) noexcept
+    {
+        if (str.is_empty() or is_empty()) return false;
+
+        return contains(str.as_str(), str.size());
+    }
+    template <std::size_t length>
+    void string<length>::erase(std::size_t start, std::size_t size) noexcept
+    {
+        if (is_empty() or start >= this->size()) return;
+
+        size_t end_index = start + size;
+
+        if (end_index >= this->size()) {
+            _size = start;
+        } else {
+            size_t count_move = this->size() - end_index + 1;
+            memmove(&_data[start], &_data[end_index], count_move);
+            _size -= size;
+        }
+
+        terminate();
+    }
+
+    template <std::size_t length>
+    bool string<length>::equal(char ch) noexcept
+    {
+        if (is_empty() or size() > 1) return false;
+
+        return _data[0] == ch;
+    }
+
+    template<std::size_t length>
+    bool string<length>::equal(const char* str) noexcept
+    {
+        std::size_t len = strlen(str);
+
+        if (len == 0 and is_empty()) return true;
+
+        return strcmp(as_str(), str) == 0;
+    }
+
+    template<std::size_t length>
+    template<std::size_t equal_length>
+    bool string<length>::equal(const string<equal_length>& str) noexcept
+    {
+        if (str.is_empty() and is_empty()) return true;
+
+        return strcmp(as_str(), str.as_str()) == 0;
+    }
 
     template <std::size_t length>
     auto operator+(string<length>& lhs, char rhs) noexcept -> string<length>&
@@ -191,11 +316,42 @@ namespace data
     }
 
     template <std::size_t left_length, std::size_t right_length>
-    auto operator+(string<left_length>& lhs, string<right_length> rhs)
+    auto operator+(string<left_length>& lhs, string<right_length> rhs) noexcept
         -> string<left_length>&
     {
         lhs.append(rhs);
         return lhs;
+    }
+
+    template<std::size_t length>
+    bool operator==(string<length>& lhs, char rhs) noexcept
+    {
+        return lhs.equal(rhs);
+    }
+
+    template <std::size_t length>
+    bool operator==(char lhs, string<length>& rhs) noexcept
+    {
+        return rhs.equal(lhs);
+    }
+
+    template <std::size_t length>
+    bool operator==(string<length>& lhs, const char* rhs) noexcept
+    {
+        return lhs.equal(rhs);
+    }
+
+    template <std::size_t length>
+    bool operator==(const char* lhs, string<length>& rhs) noexcept
+    {
+        return rhs.equal(lhs);
+    }
+
+    template <std::size_t left_length, std::size_t right_length>
+    bool operator==(string<left_length>& lhs,
+                    string<right_length>& rhs) noexcept
+    {
+        return lhs.equal(rhs);
     }
 
 }  // namespace data
--- tests/data/string_test.cpp
+++ tests/data/string_test.cpp
@@ -3,22 +3,35 @@
 //
 
 #include <gtest/gtest.h>
+
+#define protected public
+#define private public
 #include <lib/data/string.hpp>
+#undef protected
+#undef private
 
 TEST(string_test, create)
 {
-    const std::size_t length = 16;
+    const std::size_t length = 4;
     using string_t = lib::data::string<length>;
 
-    string_t str;
+    {
+        string_t str;
 
-    EXPECT_EQ(str.capacity(), length) << "Capacity not equal length";
-    EXPECT_EQ(str.size(), 0) << "Initial size not 0";
+        EXPECT_EQ(str.capacity(), length) << "Capacity not equal length";
+        EXPECT_EQ(str.size(), 0) << "Initial size not 0";
 
-    str.append('A');
-    str.append('B');
+        str.append('A');
+        str.append('B');
 
-    EXPECT_EQ(str.free(), 14) << "Error calculate free size";
+        EXPECT_EQ(str.free(), 2) << "Error calculate free size";
+    }
+
+    {
+        string_t str("ABC");
+
+        EXPECT_EQ(str.size(), 3) << "Created size not equal actual string size";
+    }
 }
 
 TEST(string_test, copy)
@@ -26,8 +39,8 @@ TEST(string_test, copy)
     using namespace lib::data;
 
     {
-        string<3> to_copy = "";
-        string<4> from_copy = "AB";
+        string<3> to_copy("");
+        string<4> from_copy("AB");
 
         to_copy = from_copy;
 
@@ -35,7 +48,7 @@ TEST(string_test, copy)
     }
 
     {
-        string<4> from_copy = "AB";
+        string<4> from_copy("AB");
         string<3> to_copy(from_copy);
 
         EXPECT_STREQ(to_copy.as_str(), "AB")
@@ -44,20 +57,18 @@ TEST(string_test, copy)
 
     {
         const char* from_copy = "AB";
-        string<3> to_copy = "";
+        string<3> to_copy;
 
         to_copy = from_copy;
 
-        EXPECT_STREQ(to_copy.as_str(), "AB")
-            << "Error copy operator for str";
+        EXPECT_STREQ(to_copy.as_str(), "AB") << "Error copy operator for str";
     }
 
     {
         const char* from_copy = "AB";
         string<3> to_copy(from_copy);
 
-        EXPECT_STREQ(to_copy.as_str(), "AB")
-            << "Error copy constructor by str";
+        EXPECT_STREQ(to_copy.as_str(), "AB") << "Error copy constructor by str";
     }
 }
 
@@ -107,7 +118,7 @@ TEST(string_test, operators)
     const std::size_t size = 3;
 
     {
-        string<size> data = "";
+        string<size> data;
         data = data + 'A';
         data = data + 'B';
 
@@ -115,7 +126,7 @@ TEST(string_test, operators)
     }
 
     {
-        string<size> data = "";
+        string<size> data;
         data = 'A' + data;
         data = 'B' + data;
 
@@ -123,7 +134,7 @@ TEST(string_test, operators)
     }
 
     {
-        string<size> data = "";
+        string<size> data;
         data = data + "A";
         data = data + "B";
 
@@ -131,7 +142,7 @@ TEST(string_test, operators)
     }
 
     {
-        string<size> data = "";
+        string<size> data;
         data = "A" + data;
         data = "B" + data;
 
@@ -139,13 +150,150 @@ TEST(string_test, operators)
     }
 
     {
-        string<size> data = "";
-        string<2> data_append_1 = "A";
-        string<2> data_append_2 = "B";
+        string<size> data;
+        string<2> data_append_1("A");
+        string<2> data_append_2("B");
 
         data = data + data_append_1;
         data = data + data_append_2;
 
         EXPECT_STREQ(data.as_str(), "AB");
+    }
+}
+
+TEST(string_test, starts_with)
+{
+    using namespace lib::data;
+
+    {
+        string<3> data("A");
+        string<3> empty_data;
+
+        EXPECT_TRUE(data.starts_with('A'));
+        EXPECT_FALSE(data.starts_with('B'));
+        EXPECT_FALSE(empty_data.starts_with('A'));
+    }
+
+    {
+        string<3> data("AB");
+        string<3> empty_data;
+        const char* compare_data_1 = "ABC";
+        const char* compare_data_2 = "AB";
+
+        EXPECT_FALSE(data.starts_with(compare_data_1));
+        EXPECT_TRUE(data.starts_with(compare_data_2));
+        EXPECT_FALSE(empty_data.starts_with(compare_data_2));
+    }
+
+    {
+        string<6> data("ABCDE");
+        string<4> compare_data_1("ABC");
+        string<4> compare_data_2("ABE");
+
+        EXPECT_TRUE(data.starts_with(compare_data_1));
+        EXPECT_FALSE(data.starts_with(compare_data_2));
+    }
+}
+
+TEST(string_test, contains)
+{
+    using namespace lib::data;
+    {
+        string<8> data("ABCDEFG");
+        const char* contains_data_1 = "BC";
+        const char* contains_data_2 = "XZ";
+        const char* contains_data_3 = "GH";
+
+        EXPECT_TRUE(data.contains(contains_data_1, 2));
+        EXPECT_FALSE(data.contains(contains_data_2, 2));
+        EXPECT_FALSE(data.contains(contains_data_3, 2));
+    }
+
+    {
+        string<3> data("ABC");
+        string<0> empty_data;
+
+        EXPECT_TRUE(data.contains('A'));
+        EXPECT_TRUE(data.contains('B'));
+        EXPECT_TRUE(data.contains('C'));
+        EXPECT_FALSE(data.contains('F'));
+        EXPECT_FALSE(data.contains('e'));
+        EXPECT_FALSE(empty_data.contains("A"));
+    }
+
+    {
+        string<16> data("Hello, there!!!");
+
+        EXPECT_TRUE(data.contains("Hello"));
+        EXPECT_TRUE(data.contains(", "));
+        EXPECT_TRUE(data.contains("there!!!"));
+        EXPECT_FALSE(data.contains("minute"));
+        EXPECT_FALSE(data.contains("thar"));
+        EXPECT_FALSE(data.contains("lla"));
+    }
+
+    {
+        string<16> data("Hello, there!!!");
+
+        EXPECT_TRUE(data.contains(string<10>("Hello")));
+        EXPECT_TRUE(data.contains(string<10>(", ")));
+        EXPECT_TRUE(data.contains(string<10>("there!!!")));
+        EXPECT_FALSE(data.contains(string<10>("minute")));
+        EXPECT_FALSE(data.contains(string<10>("thar")));
+        EXPECT_FALSE(data.contains(string<10>("lla")));
+    }
+}
+
+TEST(string_test, erase)
+{
+    using namespace lib::data;
+
+    string<32> data("Hello, dear friends!!!");
+
+    data.erase(7, 5);
+    EXPECT_STREQ(data.as_str(), "Hello, friends!!!");
+
+    data.erase(14, 4);
+    EXPECT_STREQ(data.as_str(), "Hello, friends");
+
+    data.erase(0, 7);
+    EXPECT_STREQ(data.as_str(), "friends");
+}
+
+TEST(string_test, equal)
+{
+    using namespace lib::data;
+    {
+        string<3> data("A");
+        string<3> wrong_data("AB");
+        string<3> empty_data;
+
+        EXPECT_TRUE(data.equal('A'));
+        EXPECT_FALSE(data.equal('B'));
+        EXPECT_FALSE(wrong_data.equal('A'));
+        EXPECT_FALSE(empty_data.equal('A'));
+
+        EXPECT_TRUE(data == 'A' and 'A' == data);
+        EXPECT_FALSE(data == 'B' or 'B' == data);
+        EXPECT_FALSE(wrong_data == 'A' or 'A' == wrong_data);
+        EXPECT_FALSE(empty_data == 'A' or 'A' == empty_data);
+    }
+
+    {
+        string<16> data("ABC EFG");
+        const char* equal_str = "ABC EFG";
+        const char* not_equal_str = "ABK EFG ";
+        string<10> equal_data(equal_str);
+        string<10> not_equal_data(not_equal_str);
+
+        EXPECT_TRUE(data.equal(equal_str));
+        EXPECT_FALSE(data.equal(not_equal_str));
+        EXPECT_TRUE(data.equal(equal_data));
+        EXPECT_FALSE(data.equal(not_equal_data));
+
+        EXPECT_TRUE(data == equal_str and equal_str == data);
+        EXPECT_FALSE(data == not_equal_str or not_equal_str == data);
+        EXPECT_TRUE(data == equal_data);
+        EXPECT_FALSE(data == not_equal_data);
     }
 }
